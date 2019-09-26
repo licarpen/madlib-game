7711317ffa8cddc6ab8a515a1d5b87defb04c5af
Lisa Carpenter <carpenter.lisa@gmail.com>
2019-09-26 16:23:33 -0700

    Working madlib version

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-function fillInBlank(){
-    const adjectiveInput = document.getElementById('adjectiveInput');
-
-    const adjective = adjectiveInput.value;
-    adjective1.textContent = adjective;
-}
+function fillInBlanks(){
+    
+    const adj1Input = document.getElementById('adj1Input');
+    const adj1Output = adj1Input.value;
+    adj1.textContent = adj1Output;
+    document.getElementById("storyResults").style.visibility = "visible";
+};
