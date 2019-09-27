92a8d1c133f3cba021faeeb3f87c7572cc4f19c4
Lisa Carpenter <carpenter.lisa@gmail.com>
2019-09-26 21:08:39 -0700

    A finished madlib

--- app.js
+++ app.js
@@ -3,5 +3,70 @@ function fillInBlanks(){
     const adj1Input = document.getElementById('adj1Input');
     const adj1Output = adj1Input.value;
     adj1.textContent = adj1Output;
+
+    const placeInput = document.getElementById('placeInput');
+    const placeOutput = placeInput.value;
+    place.textContent = placeOutput;
+
+    const adj2Input = document.getElementById('adj2Input');
+    const adj2Output = adj2Input.value;
+    adj2.textContent = adj2Output;
+
+    const feeling1Input = document.getElementById('feeling1Input');
+    const feeling1Output = feeling1Input.value;
+    feeling1.textContent = feeling1Output;
+
+    const number1Input = document.getElementById('number1Input');
+    const number1Output = number1Input.value;
+    number1.textContent = number1Output;
+
+    const verbInput = document.getElementById('verbInput');
+    const verbOutput = verbInput.value;
+    verb.textContent = verbOutput;
+
+    const animalPluralInput = document.getElementById('animalPluralInput');
+    const animalPluralOutput = animalPluralInput.value;
+    animalPlural.textContent = animalPluralOutput;
+
+    const objectPluralInput = document.getElementById('objectPluralInput');
+    const objectPluralOutput = objectPluralInput.value;
+    objectPluralOutput.textContent = objectPluralOutput;
+
+    const feeling2Input = document.getElementById('feeling2Input');
+    const feeling2Output = feeling2Input.value;
+    feeling2.textContent = feeling2Output;
+
+    const quoteInput = document.getElementById('quoteInput');
+    const quoteOutput = quoteInput.value;
+    quote.textContent = quoteOutput;
+
+    const modernNameInput = document.getElementById('modernNameInput');
+    const modernNameOutput = modernNameInput.value;
+    modernName.textContent = modernNameOutput;
+
+    const thingInput = document.getElementById('thingInput');
+    const thingOutput = thingInput.value;
+    thing.textContent = thingOutput;
+
+    const universitySubjectInput = document.getElementById('universitySubjectInput');
+    const universitySubjectOutput = universitySubjectInput.value;
+    universitySubject.textContent = universitySubjectOutput;
+
+    const adj3Input = document.getElementById('adj3Input');
+    const adj3Output = adj3Input.value;
+    adj3.textContent = adj3Output;
+
+    const bodyPartInput = document.getElementById('bodyPartInput');
+    const bodyPartOutput = bodyPartInput.value;
+    bodyPart.textContent = bodyPartOutput;
+
+    const number2Input = document.getElementById('number2Input');
+    const number2Output = number2Input.value;
+    number2.textContent = number2Output;
+
+    const occupationInput = document.getElementById('occupationInput');
+    const occupationOutput = occupationInput.value;
+    occupation.textContent = occupationOutput;
+
     document.getElementById("storyResults").style.visibility = "visible";
 };
